ebb25424d3aab76cf6026862676b02cf9445a3fd
kevinseya <kevinseya@gmail.com>
2025-01-27 14:37:21 -0500

    feat: ✨ Create project of microservice create with Go

--- routes/routes.go
+++ routes/routes.go
@@ -7,7 +7,6 @@ import (
 
 func SetupRoutes(router *gin.Engine) {
 
-	// Rutas de la API
 	api := router.Group("/api/shippment")
 	{
 		api.POST("/create", controllers.CreateShipment)   // Crear un envío
